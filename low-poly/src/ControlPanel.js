8f792b5cccbf5188cd500d8b93db8f56164ca4f0
Lunarelements <bradlabrador@hotmail.com>
2021-10-24 02:11:26 -0400

    Working state management

--- low-poly/src/ControlPanel.js
+++ low-poly/src/ControlPanel.js
@@ -1,21 +1,34 @@
 import { useForm } from "react-hook-form";
 import Card from "./Card"
 import NumberInput from "./NumberInput"
+import useHexagonStore from "./store/useHexagonStore"
 import SubmitButton from "./SubmitButton"
 import TextInput from "./TextInput"
 
 const App = () => {
-  const { handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const { handleSubmit, register } = useForm();
+
+  const lineColor = useHexagonStore(state => state.lineColor)
+  const setLineColor = useHexagonStore(state => state.setLineColor)
+  const backgroundColor = useHexagonStore(state => state.backgroundColor)
+  const setBackgroundColor = useHexagonStore(state => state.setBackgroundColor)
+  const strokeWidth = useHexagonStore(state => state.strokeWidth)
+  const setStrokeWidth = useHexagonStore(state => state.setStrokeWidth)
+
+  const onSubmit = (data) => {
+    setLineColor(data["line-color"])
+    setBackgroundColor(data["background-color"])
+    setStrokeWidth(parseInt(data["stroke-width"]))
+  }
 
   return (
     <div className="flex-1 m-4">
       <Card>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="w-full mb-4">
-            <TextInput label="Line Color" id="line-color" defaultValue="#000000" />
-            <TextInput label="Background Color" id="background-color" defaultValue="#FFFFFF" />
-            <NumberInput label="Stroke Width" id="stroke-width" defaultValue="5" />
+            <TextInput label="Line Color" id="line-color" defaultValue={lineColor} register={register} />
+            <TextInput label="Background Color" id="background-color" defaultValue={backgroundColor} register={register} />
+            <NumberInput label="Stroke Width" id="stroke-width" defaultValue={strokeWidth} register={register} />
           </div>
           <div className="flex justify-center mt-2">
             <SubmitButton />
--- low-poly/src/NumberInput.js
+++ low-poly/src/NumberInput.js
@@ -1,12 +1,9 @@
-import { useForm } from "react-hook-form";
 
-const NumberInput = ({label, id, defaultValue}) => {
-  const { register } = useForm();
-
+const NumberInput = ({label, id, defaultValue, register}) => {
   return (
     <div className="mb-4">
       <label htmlFor="line-color" className="block text-gray-700 text-sm font-bold mb-2">{label}</label>
-      <input defaultValue={defaultValue} {...register(id)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      <input type="number" defaultValue={defaultValue} {...register(id)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
   );
 }
