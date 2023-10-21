4ca7d97b2357a21dba0317c6f397772114a93a51
Technickel-Dev <bradlabrador@hotmail.com>
2023-10-20 23:03:52 -0400

    Upgrade all the libraries

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import "./index.css";
+import App from "./components/App";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
--- src/store/useHexagonStore.js
+++ src/store/useHexagonStore.js
@@ -1,26 +1,35 @@
-import create from 'zustand';
-import { devtools } from 'zustand/middleware';
-import produce from 'immer'
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { produce } from "immer";
 
 const store = (set) => ({
-  controlData: { 
-    lineColor: '#000000',
-    backgroundColor: '#FFFFFF',
+  controlData: {
+    lineColor: "#000000",
+    backgroundColor: "#FFFFFF",
     strokeWidth: 5,
     blurKernal: 3,
-    erosionKernal: 3
+    erosionKernal: 3,
   },
-  dataPath: '125 125 125 225 225 225 225 125',
+  dataPath: "125 125 125 225 225 225 225 125",
   fileURL: null,
-  setControlData: (controlData) => set(produce(state => {
-    state.controlData = controlData
-  })),
-  setDataPath: (dataPath) => set(produce(state => {
-    state.dataPath = dataPath
-  })),
-  setFileURL: (fileURL) => set(produce(state => {
-    state.fileURL = fileURL
-  })),
+  setControlData: (controlData) =>
+    set(
+      produce((state) => {
+        state.controlData = controlData;
+      })
+    ),
+  setDataPath: (dataPath) =>
+    set(
+      produce((state) => {
+        state.dataPath = dataPath;
+      })
+    ),
+  setFileURL: (fileURL) =>
+    set(
+      produce((state) => {
+        state.fileURL = fileURL;
+      })
+    ),
 });
 
 const useHexagonStore = create(devtools(store));
