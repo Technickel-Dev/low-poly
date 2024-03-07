6a27218f656a3f1acdbb37d1a55fb27abdd7c3b5
Technickel-Dev <bradlabrador@hotmail.com>
2024-03-06 22:42:59 -0500

    Use Vite instead of react-scripts

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import "./index.css";
-import App from "./components/App";
-import ReactDOM from "react-dom/client";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
